bae8341af5d5744dd7d3624d9b9d82a69005cecd
Τιμόθεος 🧨 <161139825+pipa53@users.noreply.github.com>
2025-04-10 16:49:12 +0300

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -5,125 +5,107 @@
 #include <string>
 
 template <typename T>
-struct CreateUsingNew
-{
-	static T *Create()
-	{
-		return new T
-	}
-	static void Destroy(T *p)
-	{
-		delete p
-	}
-}
+struct CreateUsingNew {
+    static T* Create() {
+        return new T;
+    }
+    static void Destroy(T* p) {
+        delete p;
+    }
+};
 
 template <typename T>
-struct CreateUsingShared
-{
-	static std::shared_ptr<T> Create()
-	{
-		return std::make_shared<T>()
-	}
-}
+struct CreateUsingShared {
+    static std::shared_ptr<T> Create() {
+        return std::make_shared<T>();
+    }
+   
+    static void Destroy(std::shared_ptr<T>) {
+        
+    }
+};
 
-struct NoLock
-{
-	void lock() {}
-	void unlock() {}
-}
+struct NoLock {
+    void lock() {}
+    void unlock() {}
+};
 
-struct MutexLock
-{
-	void lock()
-	{
-		m.lock()
-	}
-	void unlock()
-	{
-		m.unlock()
-	}
+struct MutexLock {
+    void lock() { m.lock(); }
+    void unlock() { m.unlock(); }
 private:
-	std::mutex m
-}
+    std::mutex m;
+};
 
 template <
-	typename T,
-	template <typename> class CreationPolicy,
-	typename LockPolicy>
-class Singleton
-{
+    typename T,
+    template <typename> class CreationPolicy = CreateUsingNew,
+    typename LockPolicy = MutexLock>
+class Singleton {
 public:
-	static T &Instance()
-	{
-		l.lock()
-		if (!instance)
-			instance = CreationPolicy<T>::Create()
-		l.unlock()
-		return *instance
-	}
+    static T& Instance() {
+        std::lock_guard<LockPolicy> guard(l);
+        if (!instance) {
+            instance = CreationPolicy<T>::Create();
+        }
+        return *instance;
+    }
 
-	static void Destroy()
-	{
-		l.lock()
-		if (instance)
-		{
-			CreationPolicy<T>::Destroy(instance)
-			instance = nullptr
-		}
-		l.unlock()
-	}
-private:
-	static inline T *instance = nullptr
-	static inline LockPolicy l
-	Singleton() = delete
-	Singleton(const Singleton &) = delete
-	Singleton &operator=(const Singleton &) = delete
-}
+    static void Destroy() {
+        std::lock_guard<LockPolicy> guard(l);
+        if (instance) {
+            CreationPolicy<T>::Destroy(instance);
+            instance = nullptr;
+        }
+    }
 
+protected:
+    static inline T* instance = nullptr;
+    static inline LockPolicy l;
 
-class Logger
-{
+    Singleton() = delete;
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+};
+
+class Logger {
 public:
-	void SetOutputFile(const std::string &filename)
-	{
-		l.lock()
-		if (ofs.is_open())
-			ofs.close()
-		ofs.open(filename, std::ios::app)
-		l.unlock()
-	}
+    void SetOutputFile(const std::string& filename) {
+        std::lock_guard<std::mutex> guard(lock);
+        if (ofs.is_open())
+            ofs.close();
+        ofs.open(filename, std::ios::app);
+    }
 
-	void Log(const std::string &msg)
-	{
-		l.lock()
-		if (ofs.is_open())
-			ofs << msg << '\n'
-		else
-			std::cout << msg << '\n'
-		l.unlock()
-	}
+    void Log(const std::string& msg) {
+        std::lock_guard<std::mutex> guard(lock);
+        if (ofs.is_open())
+            ofs << msg << '\n';
+        else
+            std::cout << msg << '\n';
+    }
 
-	~Logger()
-	{
-		l.lock()
-		if (ofs.is_open())
-			ofs.close()
-		l.unlock()
-	}
+    ~Logger() {
+        std::lock_guard<std::mutex> guard(lock);
+        if (ofs.is_open())
+            ofs.close();
+    }
+
 private:
-	Logger() = default
-	std::ofstream ofs
-	std::mutex l
-	friend struct CreateUsingNew<Logger>
-}
+    Logger() = default;
 
-using AppLogger = Singleton<Logger, CreateUsingNew, MutexLock>
+    std::ofstream ofs;
+    std::mutex lock;
 
-int main()
-{
-	auto &log = AppLogger::Instance()
-	log.SetOutputFile("log.txt")
-	log.Log("Starting application")
-	log.Log("Logging to file")
-	AppLogger::Destroy()
+    friend struct CreateUsingNew<Logger>;
+};
+
+using AppLogger = Singleton<Logger, CreateUsingNew, MutexLock>;
+
+int main() {
+    auto& log = AppLogger::Instance();
+    log.SetOutputFile("log.txt");
+    log.Log("Запуск приложения");
+    log.Log("Логируем в файл");
+    AppLogger::Destroy();
 }
